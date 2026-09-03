28a625c60fb0f99269ea460b6cc8a68ffcfeb456
Ankur-Dwivedi22 <139616073+Ankur-Dwivedi22@users.noreply.github.com>
2026-09-03 23:12:05 +0530

    01. Minimum Path Sum.cpp

--- 26. Dynamic Programming Part-II/01. Minimum Path Sum.cpp
+++ 26. Dynamic Programming Part-II/01. Minimum Path Sum.cpp
@@ -1,4 +1,42 @@
+// Counting minimum paths
+
+class Solution {
+public:
+    const long long M = 1e9 + 7;
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        vector<vector<long long>> ways(m, vector<long long>(n, 0));
+        dp[0][0] = grid[0][0];
+        ways[0][0] = 1;
+
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i == 0 && j == 0) continue;
+                int up = 1e9, left = 1e9;
+                if(i-1 >= 0) up = grid[i][j] + dp[i-1][j];
+                if(j-1 >= 0) left = grid[i][j] + dp[i][j-1];
+                dp[i][j] = min(up, left);
+
+                if(up < left){
+                    ways[i][j] = ways[i-1][j];
+                }
+                else if(left < up){
+                    ways[i][j] = ways[i][j-1];
+                }
+                else{
+                    ways[i][j] = (ways[i-1][j] + ways[i][j-1]) % M;
+                }
+            }
+        }
+
+        cout<<ways[m-1][n-1]<<"\n";
+        return dp[m-1][n-1];
+    }
+};
+
 // Constructing minimum path
+
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
