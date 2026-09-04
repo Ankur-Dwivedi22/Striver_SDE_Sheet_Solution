51511ceb127d35bec533344db5062453cc739ad6
Ankur-Dwivedi22 <139616073+Ankur-Dwivedi22@users.noreply.github.com>
2026-09-04 18:40:43 +0530

    06. Word Break.cpp

--- 26. Dynamic Programming Part-II/06. Word Break.cpp
+++ 26. Dynamic Programming Part-II/06. Word Break.cpp
@@ -1,3 +1,7 @@
+// Memoisation 
+// TC : O(N ^ 3)
+// SC : O(N) + O(N) + O(N)
+
 class Solution {
 public:
     bool f(int ind, string &s, unordered_map<string,int> &mp, vector<int> &dp){
@@ -26,3 +30,33 @@ public:
         return f(0,s,mp,dp);
     }
 };
+
+// Tabulation
+// TC : O(N ^ 3)
+// SC : O(N) + O(N)
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        unordered_map<string, int> mp;
+        for(string &word : wordDict){
+            mp[word]++;
+        }
+
+        vector<bool> dp(n+1, 0);
+        dp[0] = 1;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<i; j++){
+                if(dp[j] && mp[s.substr(j, i-j)]){
+                    dp[i] = 1;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
+
