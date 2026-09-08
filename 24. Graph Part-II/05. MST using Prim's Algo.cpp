16acc74093cbd858779c6493eabf0d8d2858f0e2
Ankur-Dwivedi22 <139616073+Ankur-Dwivedi22@users.noreply.github.com>
2026-09-08 09:37:42 +0530

    05. MST using Prim's Algo.cpp

--- 24. Graph Part-II/05. MST using Prim's Algo.cpp
+++ 24. Graph Part-II/05. MST using Prim's Algo.cpp
@@ -39,3 +39,42 @@ class Solution {
         return sum;
     }
 };
+
+// For printing MST
+class Solution {
+  public:
+    int spanningTree(int V, vector<vector<int>>& edges) {
+        // code here
+        vector<vector<pair<int,int>>> adj(V);
+        for(auto it : edges){
+            int u = it[0], v = it[1], wt = it[2];
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, wt});
+        }
+        
+        vector<int> vis(V, 0);
+        priority_queue<vector<int>, vector<vector<int>>, greater<>> pq;
+        pq.push({0, 0, -1});
+        int sum = 0;
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            int wt = it[0], node = it[1], parent = it[2];
+            
+            if(vis[node] == 1) continue;
+            sum += wt;
+            vis[node] = 1;
+            if(parent != -1){
+                cout<<parent<<"-"<<node<<":"<<wt<<endl;
+            }
+            
+            for(auto [adjNode, edwt] : adj[node]){
+                if(!vis[adjNode]){
+                    pq.push({edwt, adjNode});
+                }
+            }
+        }
+        
+        return sum;
+    }
+};
